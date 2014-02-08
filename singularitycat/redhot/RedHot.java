f1f8cf7e26b7b040dd164de512748d6652aaa92d
Elliot Thomas <e.singularitycat@gmail.com>
2014-02-08 18:24:38 +0000

    Now reference blocks and items from net.minecraft.init.(Blocks|Items); MC 1.7.2 Removed NetworkMod annotation (not present in forge 10) Moved to log4j. Added name-related block/item calls to preinit. Renamed a load of stuff to reflect API changes.

--- singularitycat/redhot/RedHot.java
+++ singularitycat/redhot/RedHot.java
@@ -1,12 +1,17 @@
 package singularitycat.redhot;
 
-import java.util.logging.Logger;
+//import java.util.logging.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import java.util.Random;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
 
 import cpw.mods.fml.common.FMLLog;
 
@@ -17,20 +22,17 @@ import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
-import cpw.mods.fml.common.network.NetworkMod;
 
 import cpw.mods.fml.common.registry.GameRegistry;
 
-import net.minecraftforge.common.Configuration;
-import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.oredict.OreDictionary;
 
 @Mod(modid=RedHot.modid, name=RedHot.name, version=RedHot.version)
-@NetworkMod(clientSideRequired=true, serverSideRequired=false)
 public class RedHot {
     public static final String modid="redhot";
     public static final String name="Red Hot";
-    public static final String version="0.0.6";
+    public static final String version="0.0.7";
 
     public static final Random RNG = new Random(); 
     public static Configuration config;
@@ -52,16 +54,28 @@ public class RedHot {
     @EventHandler
     public void preInit(FMLPreInitializationEvent event)
     {
-        logger = Logger.getLogger(modid);
-        logger.setParent(FMLLog.getLogger());
+    	logger = LogManager.getLogger(modid);
         
         config = new Configuration(event.getSuggestedConfigurationFile());
         config.load();
         
-        glowstoneBricks = new GlowstoneBricks(config.getBlock("glowstoneBricks", 500).getInt());
-        impactedStone = new ImpactedStone(config.getBlock("impactedStone", 501).getInt());
-    
-        plantMass = new PlantMass(config.getItem("plantMass", 5000).getInt());
+        glowstoneBricks = new GlowstoneBricks();
+        glowstoneBricks.setBlockName("glowstoneBricks");
+        glowstoneBricks.setBlockTextureName(modid + ":" + "glowstoneBricks");
+    	GameRegistry.registerBlock(glowstoneBricks, "glowstoneBricks");
+
+        impactedStone = new ImpactedStone();
+        impactedStone.setBlockName("impactedStone");
+        impactedStone.setBlockTextureName(modid + ":" + "impactedStone");
+    	GameRegistry.registerBlock(impactedStone, "impactedStone");
+    	
+        plantMass = new PlantMass();
+        plantMass.setUnlocalizedName("plantMass");
+        plantMass.setTextureName(modid + ":" + "plantMass");
+    	GameRegistry.registerItem(plantMass, "plantMass");
+    	
+    	logger.info("Registering plant mass fuel handler");
+        GameRegistry.registerFuelHandler(new RedHotFuelHandler());
     }
 
     @EventHandler
@@ -80,20 +94,17 @@ public class RedHot {
     }
 
     public void initGlowstoneBricks()
-    {    	
-    	MinecraftForge.setBlockHarvestLevel(glowstoneBricks, "pickaxe", 0);
-    	GameRegistry.registerBlock(glowstoneBricks, "redhot_glowstoneBricks");
-
+    {
     	logger.info("Adding glowstone bricks <-> glowstone recipes");
         GameRegistry.addShapedRecipe(
             new ItemStack(glowstoneBricks, 16),
             "GG",
             "GG",
-            'G', new ItemStack(Block.glowStone)
+            'G', new ItemStack(Blocks.glowstone)
         );
 
         GameRegistry.addShapedRecipe(
-            new ItemStack(Block.glowStone, 1),
+            new ItemStack(Blocks.glowstone, 1),
             "GG",
             "GG",
             'G', new ItemStack(glowstoneBricks)
@@ -101,20 +112,17 @@ public class RedHot {
     }
     
     public void initImpactedStone()
-    {    	
-    	MinecraftForge.setBlockHarvestLevel(impactedStone, "pickaxe", 0);
-    	GameRegistry.registerBlock(impactedStone, "redhot_impactedStone");
-
+    {
     	logger.info("Adding impacted stone <-> cobblestone recipes");
         GameRegistry.addShapedRecipe(
             new ItemStack(impactedStone, 4),
             "CC",
             "CC",
-            'C', new ItemStack(Block.cobblestone)
+            'C', new ItemStack(Blocks.cobblestone)
         );
 
         GameRegistry.addShapedRecipe(
-            new ItemStack(Block.cobblestone, 4),
+            new ItemStack(Blocks.cobblestone, 4),
             "II",
             "II",
             'I', new ItemStack(impactedStone)
@@ -126,19 +134,14 @@ public class RedHot {
     	ItemStack plantMassStack = new ItemStack(plantMass);
     	
     	ItemStack biomass[] = {
-    		new ItemStack(Block.waterlily, 1, OreDictionary.WILDCARD_VALUE),
-    		new ItemStack(Block.leaves, 1, OreDictionary.WILDCARD_VALUE),
-    		new ItemStack(Block.sapling, 1, OreDictionary.WILDCARD_VALUE),
-    		new ItemStack(Block.vine, 1, OreDictionary.WILDCARD_VALUE),
-    		new ItemStack(Item.melonSeeds, 1, OreDictionary.WILDCARD_VALUE),
-    		new ItemStack(Item.pumpkinSeeds, 1, OreDictionary.WILDCARD_VALUE),
-    		new ItemStack(Item.seeds, 1, OreDictionary.WILDCARD_VALUE)
+    		new ItemStack(Blocks.waterlily, 1, OreDictionary.WILDCARD_VALUE),
+    		new ItemStack(Blocks.leaves, 1, OreDictionary.WILDCARD_VALUE),
+    		new ItemStack(Blocks.sapling, 1, OreDictionary.WILDCARD_VALUE),
+    		new ItemStack(Blocks.vine, 1, OreDictionary.WILDCARD_VALUE),
+    		new ItemStack(Items.melon_seeds, 1, OreDictionary.WILDCARD_VALUE),
+    		new ItemStack(Items.pumpkin_seeds, 1, OreDictionary.WILDCARD_VALUE),
+    		new ItemStack(Items.wheat_seeds, 1, OreDictionary.WILDCARD_VALUE)
     	};
-    	
-    	GameRegistry.registerItem(plantMass, "redhot_plantMass");
-    	
-    	logger.info("Registering plant mass fuel handler");
-        GameRegistry.registerFuelHandler(new RedHotFuelHandler());
         
         logger.info("Adding plant mass recipes.");
         for(ItemStack i : biomass)
@@ -151,7 +154,7 @@ public class RedHot {
         
         logger.info("Adding plant mass -> dirt recipe");
         GameRegistry.addShapedRecipe(
-        	new ItemStack(Block.dirt, 4),
+        	new ItemStack(Blocks.dirt, 4),
         	"PP",
         	"PP",
         	'P', plantMassStack
@@ -160,52 +163,51 @@ public class RedHot {
     
     public void initVanillaRecipes()
     {
-        ItemStack coalBlockStack = new ItemStack(Block.coalBlock, 1);
-        ItemStack leatherStack = new ItemStack(Item.leather, 1);
-        ItemStack ingotIronStack = new ItemStack(Item.ingotIron, 1);
-        ItemStack ingotGoldStack = new ItemStack(Item.ingotGold, 1);
-        ItemStack diamondStack = new ItemStack(Item.diamond, 1);
-        ItemStack clothStack = new ItemStack(Block.cloth, 1, OreDictionary.WILDCARD_VALUE);
-
+        ItemStack coalBlockStack = new ItemStack(Blocks.coal_block, 1);
+        ItemStack leatherStack = new ItemStack(Items.leather, 1);
+        ItemStack ingotIronStack = new ItemStack(Items.iron_ingot, 1);
+        ItemStack ingotGoldStack = new ItemStack(Items.gold_ingot, 1);
+        ItemStack diamondStack = new ItemStack(Items.diamond, 1);
+        ItemStack clothStack = new ItemStack(Blocks.wool, 1, OreDictionary.WILDCARD_VALUE);
 
         /* Redstone / Glowstone recipes */
         logger.info("Adding redstone recipe");
         GameRegistry.addShapelessRecipe(
-                new ItemStack(Item.redstone, 2),        /* Output */
-                new ItemStack(Item.glowstone, 1),
-                new ItemStack(Item.dyePowder, 1, 1)
+                new ItemStack(Items.redstone, 2),        /* Output */
+                new ItemStack(Items.glowstone_dust, 1),
+                new ItemStack(Items.dye, 1, 1)
             );
         logger.info("Adding glowstone recipe");     
         GameRegistry.addShapelessRecipe(
-                 new ItemStack(Item.glowstone, 2),      /* Output */
-                 new ItemStack(Item.redstone, 1),
-                 new ItemStack(Item.dyePowder, 1, 11)
+                 new ItemStack(Items.glowstone_dust, 2),      /* Output */
+                 new ItemStack(Items.redstone, 1),
+                 new ItemStack(Items.dye, 1, 11)
             );
 
         /* Lava bucket recipes */
         logger.info("Adding lava bucket recipes");      
         GameRegistry.addShapelessRecipe(
-                new ItemStack(Item.bucketLava, 1),      /* Output */
-                new ItemStack(Item.bucketEmpty, 1),
-                new ItemStack(Block.cobblestone, 1),
-                new ItemStack(Item.redstone, 1),
-                new ItemStack(Item.redstone, 1)
+                new ItemStack(Items.lava_bucket, 1),      /* Output */
+                new ItemStack(Items.bucket, 1),
+                new ItemStack(Blocks.cobblestone, 1),
+                new ItemStack(Items.redstone, 1),
+                new ItemStack(Items.redstone, 1)
             );
 
         GameRegistry.addShapelessRecipe(
-                new ItemStack(Item.bucketLava, 1),      /* Output */
-                new ItemStack(Item.bucketEmpty, 1),
-                new ItemStack(Block.stone, 1),
-                new ItemStack(Item.redstone, 1),
-                new ItemStack(Item.redstone, 1)
+                new ItemStack(Items.lava_bucket, 1),      /* Output */
+                new ItemStack(Items.bucket, 1),
+                new ItemStack(Blocks.stone, 1),
+                new ItemStack(Items.redstone, 1),
+                new ItemStack(Items.redstone, 1)
             );
 
         /* Obsidian recipe (negates need for diamond pickaxe!) */
         logger.info("Adding obsidian recipe");
         GameRegistry.addShapelessRecipe(
-            new ItemStack(Block.obsidian, 1),           /* Output */
-            new ItemStack(Item.bucketLava.setContainerItem(Item.bucketEmpty), 1),
-            new ItemStack(Item.bucketWater.setContainerItem(Item.bucketEmpty), 1)
+            new ItemStack(Blocks.obsidian, 1),           /* Output */
+            new ItemStack(Items.lava_bucket.setContainerItem(Items.bucket), 1),
+            new ItemStack(Items.water_bucket.setContainerItem(Items.bucket), 1)
             );
 
         /* Diamond recipe */
@@ -220,7 +222,7 @@ public class RedHot {
         /* Saddle recipe */
         logger.info("Adding saddle recipe");
         GameRegistry.addRecipe(
-        	new ItemStack(Item.saddle, 1),
+        	new ItemStack(Items.saddle, 1),
             "LLL",
             "LIL",
             "I I",
@@ -231,7 +233,7 @@ public class RedHot {
         /* Horse armour recipes */
         logger.info("Adding iron horse armour recipe");
         GameRegistry.addRecipe(
-        	new ItemStack(Item.horseArmorIron, 1),
+        	new ItemStack(Items.iron_horse_armor, 1),
             "I  ",
             "ICI",
             "III",
@@ -241,7 +243,7 @@ public class RedHot {
 
         logger.info("Adding gold horse armour recipe");
         GameRegistry.addRecipe(
-        	new ItemStack(Item.horseArmorGold, 1),
+        	new ItemStack(Items.golden_horse_armor, 1),
             "G  ",
             "GCG",
             "GGG",
@@ -251,7 +253,7 @@ public class RedHot {
 
         logger.info("Adding diamond horse armour recipe");
         GameRegistry.addRecipe(
-        	new ItemStack(Item.horseArmorDiamond, 1),
+        	new ItemStack(Items.diamond_horse_armor, 1),
             "D  ",
             "DCD",
             "DDD",
@@ -262,7 +264,7 @@ public class RedHot {
         if(config.get("recipes", "RottenFleshToLeather", true).getBoolean(true))
         {
         	logger.info("Adding rotten flesh -> leather smelting recipe");
-        	GameRegistry.addSmelting(Item.rottenFlesh.itemID, leatherStack, 0.2f);
+        	GameRegistry.addSmelting(Items.rotten_flesh, leatherStack, 0.2f);
         }
     }
 }
